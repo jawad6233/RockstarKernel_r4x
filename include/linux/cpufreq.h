fb863940cb6de3a8957c7e475875e2571b8ff2d5
Shaka Huang <shakalaca@gmail.com>
2017-07-22 23:12:36 +0800

    Adding intelliactive gov

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -563,6 +563,10 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DANCEDANCE)
 extern struct cpufreq_governor cpufreq_gov_dancedance;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_dancedance)
+
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
+extern struct cpufreq_governor cpufreq_gov_intelliactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intelliactive)
 #endif
 
 /*********************************************************************
