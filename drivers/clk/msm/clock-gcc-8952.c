f269d9d31cbe9b70a1df1c80c02e99a76d1ad47e
dhruv <dhruvgera61@gmail.com>
2018-11-07 10:46:33 +0000

    Revert "Gpu freq to 605mhz"
    This reverts commit ca5ce207b6a76a0df2ba7a948bcc5864d3fc5c6c.

--- drivers/clk/msm/clock-gcc-8952.c
+++ drivers/clk/msm/clock-gcc-8952.c
@@ -768,8 +768,7 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_clk_8937_475MHz[] = {
 	F_SLEW( 450000000, 900000000,	  gpll3,	1,	0,	0),
 	F_SLEW( 500000000, 1000000000,	  gpll3,	1,	0,	0),
 	F_SLEW( 550000000, 1100000000,	  gpll3,	1,	0,	0),
-	F_SLEW( 605000000, 1210000000,    gpll3,        1,      0,      0),
-
+	F_SLEW( 600000000, 1200000000,	  gpll3,	1,	0,	0),
 	F_END
 };
 
@@ -4238,11 +4237,9 @@ static void override_for_8937(int speed_bin)
 
 	if (speed_bin) {
 		OVERRIDE_FMAX6(gfx3d,
-
 			LOWER, 160000000, LOW, 240000000,
 			NOMINAL, 320000000, NOM_PLUS, 400000000,
-			HIGH, 500000000, SUPER_TUR, 605000000);
-
+			HIGH, 500000000, SUPER_TUR, 600000000);
 		OVERRIDE_FTABLE(gfx3d, ftbl_gcc_oxili_gfx3d_clk, 8937_475MHz);
 	} else {
 		OVERRIDE_FMAX5(gfx3d,
@@ -4431,7 +4428,7 @@ static int msm_gcc_probe(struct platform_device *pdev)
 					ftbl_gcc_oxili_gfx3d_clk_8937_475MHz;
 				gfx3d_clk_src.c.fmax[VDD_DIG_SUPER_TUR] =
 
-								605000000;
+								600000000;
 
 			}
 		}
