4533e3f098ac14afe0138169d51c4286a8530166
Dhruvgera <42107491+Dhruvgera@users.noreply.github.com>
2019-01-30 12:56:26 +0530

    Update clang.sh

--- clang.sh
+++ clang.sh
@@ -23,7 +23,7 @@ function transfer() {
 	url="$(curl -# -T $1 https://transfer.sh)";
 	printf '\n';
 	echo -e "Download ${zipname} at ${url}";
-#    curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="$url" -d chat_id="-1001304675095"
+curl -s -X POST https://api.telegram.org/bot$BOT_API_KEY/sendMessage -d text="$url" -d chat_id="-1001304675095"
 }
 
 if [[ -z ${KERNELDIR} ]]; then
